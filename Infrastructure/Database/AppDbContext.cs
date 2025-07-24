31ed47c2b39a887291065aa351faf2a6664d1d6a
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-07-24 17:10:40 +0700

    feat: db context config

--- Infrastructure/Database/AppDbContext.cs
+++ Infrastructure/Database/AppDbContext.cs
@@ -6,7 +6,32 @@ namespace Backend.Infrastructure.Database
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-   
+        public DbSet<Community> Communities { get; set; }
+        public DbSet<CommunityMember> CommunityMembers { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+ 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Id);
+            
+            modelBuilder.Entity<User>()
+                .HasMany<Schedule>(u => u.Schedules)
+                .WithOne(s => s.Volunteer)
+                .HasForeignKey(s => s.VolunteerId)
+                .IsRequired();
+            
+            modelBuilder.Entity<User>()
+                .HasMany<CommunityMember>(u => u.Members)
+                .WithOne(s => s.User)
+                .HasForeignKey(s => s.UserId)
+                .IsRequired();
+
+            modelBuilder.Entity<Community>()
+                .HasKey(c => c.Id);
+        }
     }
 } 
