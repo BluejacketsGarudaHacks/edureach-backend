3e6dc572b46d6a4f08fb86fb929aac9876e56211
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-07-24 18:38:31 +0700

    fix: cannot create model with date time

--- Models/ModelWithDate.cs
+++ Models/ModelWithDate.cs
@@ -4,7 +4,7 @@ namespace Backend.Models;
 
 public class ModelWithDate
 {
-    [Required] public DateTime CreatedAt { get; set; } = DateTime.Now;
+    [Required] public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     [Required]
-    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
